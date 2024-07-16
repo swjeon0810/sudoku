db45aacd8ccf24bd158cc5aac4dc013920cd44c5
전세원 <jsaewon@code1.co.kr>
2024-07-16 17:53:14 +0900

    fix

--- app/components/FinishModal.tsx
+++ app/components/FinishModal.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-type ResetModalType = {
-  setShowResetModal: any;
-  reset: any;
-};
-export default function ResetModal({
-  setShowResetModal,
-  reset,
-}: ResetModalType) {
+// type FinishModalType = {
+//   setFinishGame: any;
+// };
+export default function FinishModal() {
   return (
-    <div className="flex justify-center absolute z-10 h-screen w-full bg-white dark:bg-gray-950 bg-opacity-90">
+    <div className="flex justify-center absolute z-10 h-screen w-full bg-gray-950 bg-opacity-90">
       <div className="h-72 border rounded-lg shadow relative max-w-sm">
         <div className="flex justify-end p-2">
           <button
@@ -45,23 +41,19 @@ export default function ResetModal({
               d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             ></path>
           </svg>
-          <h3 className="text-lg font-normal text-black dark:text-gray-300 mt-5 mb-6">
-            Would you like to restart the game?
+          <h3 className="text-lg font-normal text-gray-300 mt-5 mb-6">
+            Success !! Wanna Start Next Game?
           </h3>
           <a
             href="#"
-            className="text-black dark:text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2"
-            onClick={() => {
-              setShowResetModal(false);
-              reset();
-            }}
+            className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2"
+            onClick={() => {}}
           >
             Yes
           </a>
           <a
             href="#"
-            className="text-gray-900 dark:text-white bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center"
-            onClick={() => setShowResetModal(false)}
+            className="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center"
           >
             No
           </a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,40 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { VscDebugRestart } from "react-icons/vsc";
 import { MdUndo, MdDarkMode, MdSunny } from "react-icons/md";
 import { TfiPencilAlt, TfiEraser } from "react-icons/tfi";
 import _ from "lodash";
 import ResetModal from "./components/ResetModal";
-import SudokuCell from "./components/Cell";
-
-const example2 = [
-  [0, 3, 0, 0, 6, 2, 0, 7, 0],
-  [2, 7, 6, 8, 1, 5, 0, 0, 9],
-  [9, 0, 0, 0, 3, 0, 2, 5, 0],
-  [0, 0, 0, 5, 8, 0, 0, 0, 7],
-  [8, 0, 7, 0, 0, 0, 9, 0, 0],
-  [0, 2, 4, 0, 0, 0, 0, 0, 0],
-  [0, 4, 9, 6, 0, 0, 1, 3, 0],
-  [6, 1, 0, 7, 4, 0, 5, 0, 0],
-  [0, 0, 8, 2, 9, 0, 0, 6, 4],
-];
-
-export type SudokuCell = {
-  row: number; // 행
-  col: number; // 열
-  value: number; // 값
-  subgrid: number; // 0~8 중 속한 그리드
-  fixed: boolean; // 고정값 여부
-  duplicated: boolean; // 중복 여부
-  memo: Set<number>; // 메모
-};
-
-type SudokuGrid = SudokuCell[][];
-const generateEmptyGrid = (): SudokuGrid => {
-  // 비어있는 네모칸들 만들기
-  const grid: SudokuGrid = [];
-  for (let row = 0; row < 9; row++) {
-    const currentRow: SudokuCell[] = [];
-    for (let col = 0; col < 9; col++) {
-      let value = example2[row][col];
-
-      currentRow.push({
-        row,
-        col,
-        value,
-        subgrid: Math.floor(row / 3) * 3 + Math.floor(col / 3),
-        fixed: value !== 0,
-        duplicated: false,
-        memo: new Set<number>(),
-      });
-    }
-    grid.push(currentRow);
-  }
-  return grid;
-};
+import Cell from "./components/Cell";
+import FinishModal from "./components/FinishModal";
+import { SudokuGrid, SudokuCell } from "./lib/type";
+import { generateEmptyGrid } from "./lib/generate";
+import { checkDuplicateCell, checkDuplicateMemo } from "./lib/checkDuplicate";
 
 export default function Home() {
-  const [dark, setDark] = useState(false);
+  const historyRef = useRef<SudokuGrid[]>([]); // 그리드 기록을 저장해둠
+  const initFocusCell = { row: -1, col: -1, value: -1, subgrid: -1 };
+  const [focusCell, setFocusCell] = useState(initFocusCell); // 포커스된 셀
   const [sudokuGrid, setSudokuGrid] = useState<SudokuGrid>(generateEmptyGrid());
   const [showResetModal, setShowResetModal] = useState(false);
   const [inputNum, setInputNum] = useState<number | null>(null);
   const [onRemove, setOnRemove] = useState(false);
   const [onMemo, setOnMemo] = useState(false);
-  const initFocusCell = { row: -1, col: -1, value: -1, subgrid: -1 };
-  const [focusCell, setFocusCell] = useState(initFocusCell); // 포커스된 셀
-  const historyRef = useRef<SudokuGrid[]>([]); // 그리드 기록을 저장해둠
   const [finishedNum, setFinishedNum] = useState<Set<number>>(new Set());
+  const [finishGame, setFinishGame] = useState(false); // 게임 완료 여부
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
 
   const handleResetBtn = () => {
     setShowResetModal(true);
@@ -81,19 +52,19 @@ export default function Home() {
     const lastest = historyRef.current.pop();
     lastest != undefined && setSudokuGrid(lastest);
   };
+
+  /////////////////////셀 클릭 이벤트
   const cellClickEvent = (cell: SudokuCell) => {
-    //셀 클릭 이벤트
-    const { row, col, fixed, value } = cell;
+    const { row, col, fixed, value, memo } = cell;
+    setFocusCell(cell);
 
     if (fixed) {
       if (onMemo || onRemove) return;
       else {
-        setInputNum(-1);
+        setInputNum(null);
         return;
       }
     }
-
-    setFocusCell(cell);
 
     if (onRemove) {
       // "제거 모드인 경우"
@@ -105,160 +76,132 @@ export default function Home() {
 
     if (inputNum === value) {
       // 똑같은 값이 들어가있는 경우 지우기
+
       remove(row, col);
     } else {
       // 새로운 값 입력
-      updateGrid(cell, inputNum);
-    }
-  };
-  const updateGrid = (cell: SudokuCell, newValue: number) => {
-    //셀 값 변경
-    const { row, col, value, memo } = cell;
 
-    historyRef.current.push(_.cloneDeep(sudokuGrid));
+      historyRef.current.push(_.cloneDeep(sudokuGrid));
 
-    setSudokuGrid((prevGrid) => {
-      const newGrid: SudokuGrid = [...prevGrid]; // 복사하여 새로운 배열 생성
+      setSudokuGrid((prevGrid) => {
+        const newGrid: SudokuGrid = [...prevGrid]; // 복사하여 새로운 배열 생성
 
-      if (onMemo) {
-        // 메모 모드일경우
-        if (memo.has(newValue)) {
-          memo.delete(newValue);
-          //메모에서 숫자 제거
-          newGrid[row][col] = {
-            ...newGrid[row][col],
-            memo: memo,
-          };
+        if (onMemo) {
+          // 메모 모드일경우
+          if (memo.has(inputNum)) {
+            memo.delete(inputNum);
+            //메모에서 숫자 제거
+            newGrid[row][col] = {
+              ...newGrid[row][col],
+              memo: memo,
+            };
+          } else {
+            //메모에 숫자 추가
+            newGrid[row][col] = {
+              ...newGrid[row][col],
+              memo: memo.add(inputNum),
+            };
+          }
         } else {
-          //메모에 숫자 추가
+          // 메모 모드 아닐 경우
           newGrid[row][col] = {
             ...newGrid[row][col],
-            memo: memo.add(newValue),
+            value: inputNum,
           };
-        }
-      } else {
-        // 메모 모드 아닐 경우
-        newGrid[row][col] = {
-          ...newGrid[row][col],
-          value: newValue,
-        };
 
-        //입력한 값 유효성 검사. 연관 그리드에 중복된 값이 있는지 체크.
-        const duplicateCells: SudokuCell[] = checkDuplicateCell(
-          newGrid,
-          row,
-          col,
-          newValue
-        );
+          //입력한 값 유효성 검사. 연관 그리드에 중복된 값이 있는지 체크.
+          const duplicateCells: SudokuCell[] = checkDuplicateCell(
+            newGrid,
+            row,
+            col,
+            inputNum
+          );
 
-        if (duplicateCells.length > 1) {
-          // 중복되는 셀이 하나인 경우는 자기 자신이므로 넘긴다.
-          // 여러 개인 경우 duplicated 표시함.
-          duplicateCells.map(({ row, col }) => {
-            newGrid[row][col] = { ...newGrid[row][col], duplicated: true };
+          if (duplicateCells.length > 1) {
+            // 중복되는 셀이 하나인 경우는 자기 자신이므로 넘긴다.
+            // 여러 개인 경우 duplicated 표시함.
+            duplicateCells.map(({ row, col }) => {
+              newGrid[row][col] = { ...newGrid[row][col], duplicated: true };
+            });
+          }
+
+          // 연관 그리드 메모에서 해당 값을 지운다.
+          const duplicateCellsMemo: SudokuCell[] = checkDuplicateMemo(
+            newGrid,
+            row,
+            col,
+            inputNum
+          );
+
+          duplicateCellsMemo.map(({ row, col, memo }) => {
+            memo.delete(inputNum);
+            newGrid[row][col] = { ...newGrid[row][col], memo };
           });
         }
 
-        // 연관 그리드 메모에서 해당 값을 지운다.
-        const duplicateCellsMemo: SudokuCell[] = checkDuplicateMemo(
-          newGrid,
-          row,
-          col,
-          newValue
-        );
+        // 해당 숫자가 9개 이상 입력된 경우 더이상 입력하지 못하도록 비활성화.
+        const countOfInputNum = countNumber(newGrid, inputNum);
+        if (countOfInputNum >= 9) setFinishedNum((prev) => prev.add(inputNum));
+        console.log(`finishedNum : ${finishedNum}`);
 
-        duplicateCellsMemo.map(({ row, col, memo }) => {
-          memo.delete(newValue);
-          newGrid[row][col] = { ...newGrid[row][col], memo };
-        });
-      }
-      const countOfInputNum = countNumber(newGrid, newValue);
-      if (countOfInputNum >= 9) setFinishedNum((prev) => prev.add(newValue));
-      console.log(`finishedNum : ${finishedNum}`);
-      return newGrid;
+        //
+        validate(newGrid);
+
+        return newGrid;
+      });
+    }
+  };
+
+  const validate = (grid: SudokuGrid) => {
+    // 현재 그리드를 검증한다. 중복값이 없고, 숫자가 모두 9개씩 입력되었으면 문제 없음 => 정답!
+    let duplicateCnt = 0;
+    let emptyCnt = 0;
+    grid.map((row) => {
+      row.map((cell) => {
+        if (cell.duplicated) {
+          duplicateCnt++;
+        }
+        if (cell.value < 1) {
+          emptyCnt++;
+        }
+      });
     });
+    console.log(`{ 중복 개수: ${duplicateCnt}, 비어있는 값: ${emptyCnt}}`);
 
-    // 입력한 값의 개수가 9 이상일 경우.
+    if (duplicateCnt === 0 && emptyCnt === 0) {
+      //둘다 0 일 경우 정답!
+      setFinishGame(true);
+    } else return;
   };
 
   const remove = (row: number, col: number) => {
     // 숫자 지우기
-    if (onMemo) {
-      setSudokuGrid((prevGrid) => {
-        const newGrid: SudokuGrid = [...prevGrid]; // 복사하여 새로운 배열 생성
+    setSudokuGrid((prevGrid) => {
+      const newGrid: SudokuGrid = [...prevGrid]; // 복사하여 새로운 배열 생성
+
+      if (onMemo) {
+        // 메모모드인 경우 메모에서 제거하기
+
         const newMemo = newGrid[row][col].memo;
         inputNum && newMemo.delete(inputNum);
         newGrid[row][col] = {
           ...newGrid[row][col],
           memo: newMemo,
         };
+      } else {
+        // 메모모드 아닌경우, 해당 값을 지우기
 
-        return newGrid;
-      });
-    } else {
-      setSudokuGrid((prevGrid) => {
-        const newGrid: SudokuGrid = [...prevGrid]; // 복사하여 새로운 배열 생성
         newGrid[row][col] = {
           ...newGrid[row][col],
           value: -1,
           duplicated: false,
         };
-
-        return newGrid;
-      });
-    }
-  };
-
-  const checkDuplicateMemo = (
-    newGrid: SudokuGrid,
-    row: number,
-    col: number,
-    targetNum: number
-  ) => {
-    // 셀에 숫자를 입력할 때 연관 그리드 메모 중 중복값이 있는 지 체크한다.
-    const duplicateInRow = newGrid[row].filter((c) => c.memo.has(targetNum));
-    const cellsInCol = newGrid.map((row) => row[col]);
-    const duplicateInCol = cellsInCol.filter((c) => c.memo.has(targetNum));
-    const subgridRowStart = Math.floor(row / 3) * 3;
-    const subgridColStart = Math.floor(col / 3) * 3;
-    const duplicateInSubgrid = [];
-    for (let r = subgridRowStart; r < subgridRowStart + 3; r++) {
-      for (let c = subgridColStart; c < subgridColStart + 3; c++) {
-        newGrid[r][c].memo.has(targetNum) &&
-          duplicateInSubgrid.push(newGrid[r][c]);
       }
-    }
-    const allDuplicateCellsMemo = [
-      ...new Set([...duplicateInRow, ...duplicateInCol, ...duplicateInSubgrid]),
-    ];
 
-    return allDuplicateCellsMemo;
+      return newGrid;
+    });
   };
-  const checkDuplicateCell = (
-    newGrid: SudokuGrid,
-    row: number,
-    col: number,
-    targetNum: number
-  ) => {
-    // 셀에 숫자를 입력할 때 연관 그리드에 중복값이 있는지 체크하는 함수.
-    const duplicateInRow = newGrid[row].filter((c) => c.value == targetNum);
-    const cellsInCol = newGrid.map((row) => row[col]);
-    const duplicateInCol = cellsInCol.filter((c) => c.value == targetNum);
-    const subgridRowStart = Math.floor(row / 3) * 3;
-    const subgridColStart = Math.floor(col / 3) * 3;
-    const duplicateInSubgrid = [];
-    for (let r = subgridRowStart; r < subgridRowStart + 3; r++) {
-      for (let c = subgridColStart; c < subgridColStart + 3; c++) {
-        newGrid[r][c].value === targetNum &&
-          duplicateInSubgrid.push(newGrid[r][c]);
-      }
-    }
-    const allDuplicateCells = [
-      ...new Set([...duplicateInRow, ...duplicateInCol, ...duplicateInSubgrid]),
-    ];
-    console.log(allDuplicateCells);
-    return allDuplicateCells;
-  };
+
   const countNumber = (newGrid: SudokuGrid, num: number) => {
     //숫자를 입력할 때마다 해당 숫자가 그리드에 몇개 들어가 있는지 카운트한다.
     // 9개가 들어가 있으면 number Panel에서 비활성화 시킴.
@@ -272,12 +215,12 @@ export default function Home() {
     return cnt;
   };
   const handleNumberBarClick = (num: number) => {
-    setOnRemove(false);
     setInputNum(num); // 현재 클릭한 숫자 "쓰기 모드" 활성화
+    setFocusCell(initFocusCell);
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:bg-slate-950">
       <div className="flex flex-col z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex border gap-5 py-10">
         <p>Welcome !</p>
         {/* Toolbar */}
@@ -295,22 +238,22 @@ export default function Home() {
           </button>
           {/* 다크모드 토글*/}
           <label className="inline-flex items-center me-5 cursor-pointer">
-            <MdSunny />
+            <MdSunny className="text-black dark:text-white mx-1" />
             <input
               type="checkbox"
               value=""
               className="sr-only peer"
-              onChange={(e) => setDark(e.target.checked)}
+              onChange={toggleDarkMode}
             />
             <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-orange-300 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
-            <MdDarkMode />
+            <MdDarkMode className="text-black dark:text-white mx-1" />
           </label>
         </div>
         <div className="grid ">
           {sudokuGrid.map((row, rowIndex) => (
             <div key={rowIndex} className="grid grid-cols-9 ">
               {row.map((cell, colIndex) => (
-                <SudokuCell
+                <Cell
                   key={colIndex}
                   {...{
                     rowIndex,
@@ -333,10 +276,16 @@ export default function Home() {
             .map((_: any, index: number) => (
               <button
                 key={index}
-                className={`w-auto h-auto px-2 text-center text-white text-2xl border ${
-                  inputNum === index + 1 ? "bg-pink-600" : ""
+                className={`w-auto h-auto px-2 text-center text-black dark:text-white text-2xl border border-black dark:border-slate-500 ${
+                  inputNum === index + 1
+                    ? " bg-orange-600 dark:bg-orange-400 "
+                    : ""
                 } 
-              ${finishedNum.has(index + 1) ? " bg-gray-500 " : ""}`}
+              ${
+                finishedNum.has(index + 1)
+                  ? " bg-gray-500 dark:bg-gray-400"
+                  : ""
+              }`}
                 onClick={() => {
                   handleNumberBarClick(index + 1);
                 }}
@@ -369,6 +318,8 @@ export default function Home() {
       </div>
       {/* reset modal */}
       {showResetModal && <ResetModal {...{ setShowResetModal, reset }} />}
+      {/* Finish modal */}
+      {finishGame && <FinishModal />}
     </main>
   );
 }
