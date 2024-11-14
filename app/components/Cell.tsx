bf99062ac8e52a527ae3325dea012626c7840b44
swjeon0810 <jsaewon@gmail.com>
2024-11-14 17:59:44 +0900

    셀 색상 및 크기 수정

--- app/components/Cell.tsx
+++ app/components/Cell.tsx
@@ -26,10 +26,10 @@ export default function Cell({
     <div className="relative" onClick={() => cellClickEvent(cell)}>
       {/* 틀렸을 때 엑스자 */}
       {cell.duplicated && !cell.fixed && (
-        <IoCloseOutline className="absolute z-10 w-12 h-12 sm:w-12 sm:h-12 text-red-700 " />
+        <IoCloseOutline className="absolute z-10 w-10 h-10 sm:w-12 sm:h-12 text-red-700 " />
       )}
       {/* 메모 모드 */}
-      <div className="flex items-center grid grid-cols-3 grid-rows-3 absolute w-12 h-12 sm:w-12 sm:h-12 text-gray-200 text-center p-0.5 cursor-pointer">
+      <div className="flex items-center grid grid-cols-3 grid-rows-3 absolute w-10 h-10 sm:w-12 sm:h-12 text-gray-200 text-center p-0.5 cursor-pointer">
         {Array(9)
           .fill(0)
           .map((_: any, index: number) => (
@@ -48,22 +48,22 @@ export default function Cell({
       {/* 숫자 */}
       <input
         type="text"
-        className={`caret-transparent w-12 h-12 sm:w-12 sm:h-12 border font-bold border-white bg-opacity-50 dark:bg-opacity-80 dark:border-blue-100 text-center text-3xl sm:text-4xl cursor-pointer 
+        className={`caret-transparent w-10 h-10 sm:w-12 sm:h-12 border font-bold border-white bg-opacity-60 dark:bg-opacity-80 dark:border-blue-100 text-center text-3xl sm:text-4xl cursor-pointer 
  
-          ${ /* 2, 5번째 열 오른쪽 테두리 */ (colIndex === 2 || colIndex === 5) && " border-r-4 sm:border-r-2 "} 
-          ${ /* 2, 5번째 행 아래 테두리 */  (rowIndex === 2 || rowIndex === 5) && " border-b-4 sm:border-b-2 "}
+          ${ /* 2, 5번째 열 오른쪽 테두리 */ (colIndex === 2 || colIndex === 5) && " border-r-2 "} 
+          ${ /* 2, 5번째 행 아래 테두리 */  (rowIndex === 2 || rowIndex === 5) && " border-b-2 "}
           ${ /* 고정값인 경우 텍스트 색상 */ fixed ? " text-black dark:text-white " : " text-blue-800 dark:text-sky-400"}
           ${
             value === activatedNum ||
             (activatedNum && memo.has(activatedNum))
-              ? " bg-purple-500 dark:bg-purple-900 "
+              ? " bg-purple-400 dark:bg-purple-500 "
               : (row === focusCell?.row ||
                 col === focusCell?.col ||
                 subgrid === focusCell?.subgrid ) && state!=="memo"
               ? " bg-rose-200 dark:bg-rose-900 "
               : [1, 3, 5, 7].includes(subgrid)
-              ? " bg-slate-300 dark:bg-slate-700 "
-              : " dark:bg-slate-950 bg-slate-50" 
+              ? " bg-slate-400 dark:bg-slate-600 "
+              : " dark:bg-slate-950 bg-slate-100" 
           }
           
         `}
