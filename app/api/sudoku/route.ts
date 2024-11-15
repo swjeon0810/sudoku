033c21d0b50e25049d225e759168a44560a945e6
swjeon0810 <jsaewon@gmail.com>
2024-11-15 11:23:01 +0900

    캐시 헤더 추가

--- app/api/sudoku/route.ts
+++ app/api/sudoku/route.ts
@@ -1,21 +1,15 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-// export default async function handler(req: any, res:any) {
-//   try {
-//     const response = await axios.get('https://sudoku-game-and-api.netlify.app/api/sudoku'); // 실제 API URL로 변경
-//     console.log(response)
-//     res.status(200).json(response.data);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Error fetching sudoku puzzle' });
-//   }
-// }
 export async function GET() {
   // 난이도: easy, medium, hard, 답은 data에 담겨있음
   try {
     const response = await axios.get('https://sudoku-game-and-api.netlify.app/api/sudoku'); // 실제 API URL로 변경
-    return NextResponse.json(response.data);
+    const res = NextResponse.json(response.data);
+    res.headers.set('Cache-Control', 'no-store, must-revalidate'); // 캐시를 무효화하고 새 데이터로 요청하도록 설정
+
+    return res;
+
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: 'Error fetching sudoku puzzle' }, { status: 500 });
