f8407c5636cbf2c796c7e8feea81fec668db3b26
swjeon0810 <jsaewon@gmail.com>
2024-11-15 12:58:07 +0900

    add dynamic

--- app/api/sudoku/route.ts
+++ app/api/sudoku/route.ts
@@ -1,15 +1,11 @@
 import axios from 'axios';
-import { timeStamp } from 'console';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
 export async function GET() {
   // 난이도: easy, medium, hard, 답은 data에 담겨있음
   try {
-    const currentTime = new Date().getTime(); // Unix 타임스탬프 (밀리초 단위)
     const response = await axios.get('https://sudoku-game-and-api.netlify.app/api/sudoku', {
-      params: {
-        timestamp: currentTime,  // timestamp를 추가하여 매번 새로운 요청을 보내기
-      },
       headers: {
         'Cache-Control': 'no-cache'  // 캐시를 방지하기 위한 헤더 추가
       }
